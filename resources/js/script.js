734cca10819959330d800d09e8fe15076413c2d7
Tyler Patton <tylerp@inmotionhosting.com>
2015-07-28 16:21:19 -0700

    Clean up scripts
    Create init function for when page sections load

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,8 +5,6 @@ var $ = jQuery = require('jQuery'),
 
 (function($) {
 
-    var hub = {};
-
     // Returns viewport width (for older browsers)
     function viewportWidth() {
         var e = window, a = 'inner';
@@ -31,32 +29,6 @@ var $ = jQuery = require('jQuery'),
         });
     });
 
-    // Turn anchor links into smoothscroll
-    $(function(){
-        $('a').click(function(event){
-            var link = $(this).attr('href');
-            if (typeof link !== typeof undefined && link !== false){
-                if (link.indexOf('#') === 0){
-                    event.preventDefault();
-                    var destination = link.split('#')[1];
-
-                    if (destination !== ''){
-                        if (viewportWidth() > 991){
-                            var goToDest = $(link).offset().top - 100;
-                        } else{
-                            var goToDest = $(link).offset().top;
-                        }
-
-                        $('html,body').animate({
-                            scrollTop: goToDest
-                        }, 500);
-                        return false;
-                    }
-                }
-            }
-        });
-    });
-
     // Adds the hero div to the banners and the parallax scroll
     $(function(){
         var heroHeight,
@@ -78,14 +50,6 @@ var $ = jQuery = require('jQuery'),
     });
 
     // navigation scroll effect
-    if ($(window).scrollTop() >= 100){
-        $('.main-nav.trans-bg').css('background', 'rgba(0, 90, 126, 0.95)');
-        $('.main-nav .summer #navbar a').css('color', '#fff');
-    }
-    if (window.innerWidth >= 991 && $(window).scrollTop() < 100){
-        $('.main-nav.trans-bg').css('background', 'rgba(0, 90, 126, '+ $(window).scrollTop()*0.95/100 +')');
-        $('.main-nav .summer #navbar a').css('color', 'rgb(113, 72, 60)');
-    }
     $(function(){
         var navBar = $('.main-nav.trans-bg');
         var heroHeight = $('.hero').height();
@@ -102,6 +66,15 @@ var $ = jQuery = require('jQuery'),
             cEnd = [255, 255, 255],
             cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[1] - cStart[0]],
             tEnd = heroAction;
+
+        if ($(window).scrollTop() >= 100){
+            $('.main-nav.trans-bg').css('background', 'rgba(0, 90, 126, 0.95)');
+            $('.main-nav .summer #navbar a').css('color', '#fff');
+        }
+        if (window.innerWidth >= 991 && $(window).scrollTop() < 100){
+            $('.main-nav.trans-bg').css('background', 'rgba(0, 90, 126, '+ $(window).scrollTop()*0.95/100 +')');
+            $('.main-nav .summer #navbar a').css('color', 'rgb(113, 72, 60)');
+        }
 
         $(window).resize(function(){
             scroll = $(window).scrollTop();
@@ -152,68 +125,93 @@ var $ = jQuery = require('jQuery'),
         });
     });
 
-    var productBoxToggle = (function(){
-        $('.products a.features').click(function(){
-            $( this ).closest('div.row').siblings('div.dropdown').slideToggle();
+    var init = (function(){
+        
+        $('[data-sgh]').detectElem();
+
+        // Turn anchor links into smoothscroll
+        $(function(){
+            $('a').click(function(event){
+                var link = $(this).attr('href');
+                if (typeof link !== typeof undefined && link !== false){
+                    if (link.indexOf('#') === 0){
+                        event.preventDefault();
+                        var destination = link.split('#')[1];
+
+                        if (destination !== ''){
+                            if (viewportWidth() > 991){
+                                var goToDest = $(link).offset().top - 100;
+                            } else{
+                                var goToDest = $(link).offset().top;
+                            }
+
+                            $('html,body').animate({
+                                scrollTop: goToDest
+                            }, 500);
+                            return false;
+                        }
+                    }
+                }
+            });
         });
-    });
 
-    // Seach Domain Form Code
-    var searchDomain = (function(){
-        var domain_search_obj = $('.domain-search-button');
-        var input_obj = $('input#domain-search-input');
-        var valid_exts = ['com', 'net', 'org', 'info', 'biz', 'us'];
+        $(function(){
+            $('.products a.features').click(function(){
+                $( this ).closest('div.row').siblings('div.dropdown').slideToggle();
+            });
+        });
 
-        input_obj.keydown(function(e){
-            if (e.keyCode == 13) {
-                domain_search_obj.first().trigger('click');
+        // Seach Domain Form Code
+        $(function(){
+            var domain_search_obj = $('.domain-search-button');
+            var input_obj = $('input#domain-search-input');
+            var valid_exts = ['com', 'net', 'org', 'info', 'biz', 'us'];
+
+            input_obj.keydown(function(e){
+                if (e.keyCode == 13) {
+                    domain_search_obj.first().trigger('click');
+                    return false;
+                }
+                return true;
+            });
+
+            domain_search_obj.click(function(){
+                var input_text = input_obj.val();
+                var responseSec = $('.domain-response');
+                responseSec.removeClass('fail success');
+                if (input_text == '') {
+                    // Enter a Domain
+                    responseSec.html('Enter a Domain');
+                    responseSec.addClass('fail');
+                    input_obj.trigger('focus').select();
+                } else if (input_text.match(/^[a-z0-9]([a-z0-9\-]*[a-z0-9])*\.([a-z]{2,4}\.)*[a-z]{2,4}$/i) == null) {
+                    // Not a Domain
+                    responseSec.html('Not a Domain');
+                    responseSec.addClass('fail');
+                    input_obj.trigger('focus').select();
+                } else if (valid_exts.indexOf((input_text.split('.'))[1]) == -1) {
+                    // Invalid TLD
+                    responseSec.html('Invalid TLD');
+                    responseSec.addClass('fail');
+                    input_obj.trigger('focus').select();
+                } else {
+                    input_obj.prop('readonly', true);
+                    responseSec.html('Loading...');
+                    responseSec.addClass('loading');
+
+                    setTimeout(function(){
+                        input_obj.prop('readonly', false);
+                        responseSec.removeClass('loading');
+                        responseSec.html('<input type=\'submit\' value=\'Order Now\'></input>');
+                        responseSec.addClass('success');
+                    }, 1000);
+                }
                 return false;
-            }
-            return true;
-        });
-
-        domain_search_obj.click(function(){
-            var input_text = input_obj.val();
-            var responseSec = $('.domain-response');
-            responseSec.removeClass('fail success');
-            if (input_text == '') {
-                // Enter a Domain
-                responseSec.html('Enter a Domain');
-                responseSec.addClass('fail');
-                input_obj.trigger('focus').select();
-            } else if (input_text.match(/^[a-z0-9]([a-z0-9\-]*[a-z0-9])*\.([a-z]{2,4}\.)*[a-z]{2,4}$/i) == null) {
-                // Not a Domain
-                responseSec.html('Not a Domain');
-                responseSec.addClass('fail');
-                input_obj.trigger('focus').select();
-            } else if (valid_exts.indexOf((input_text.split('.'))[1]) == -1) {
-                // Invalid TLD
-                responseSec.html('Invalid TLD');
-                responseSec.addClass('fail');
-                input_obj.trigger('focus').select();
-            } else {
-                input_obj.prop('readonly', true);
-                responseSec.html('Loading...');
-                responseSec.addClass('loading');
-
-                setTimeout(function(){
-                    input_obj.prop('readonly', false);
-                    responseSec.removeClass('loading');
-                    responseSec.html('<input type=\'submit\' value=\'Order Now\'></input>');
-                    responseSec.addClass('success');
-                }, 1000);
-            }
-            return false;
+            });
         });
     });
 
     $(function(){
-        var init = function(){
-            $('[data-sgh]').detectElem();
-            searchDomain();
-            productBoxToggle();
-        };
-
         $( "#branding" ).load( "views/branding.html");
         $( "#icons" ).load( "views/icons.html");
         $( "#colors" ).load( "views/colors.html");
@@ -243,7 +241,7 @@ var $ = jQuery = require('jQuery'),
                 gridToggle = false;
                 target.text('show grid');
             }
-        })
-    })
+        });
+    });
 
 })(jQuery);
